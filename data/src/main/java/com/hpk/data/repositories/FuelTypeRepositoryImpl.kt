d6145971b936b2dab51a2cbb121080c712e76be2
VladyslavaBuriakovska <vladabur0710@gmail.com>
2022-11-07 09:38:42 +0200

    - add fuel type getting from api and saving to shared preferences

--- data/src/main/java/com/hpk/data/repositories/FuelTypeRepositoryImpl.kt
+++ data/src/main/java/com/hpk/data/repositories/FuelTypeRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.hpk.data.repositories
 
+import com.hpk.data.api.models.responses.fuel.FuelTypeResponse
 import com.hpk.data.extensions.mapToApiErrors
 import com.hpk.data.providers.FuelTypeProvider
 import com.hpk.data.services.FuelTypeService
@@ -10,23 +11,19 @@ class FuelTypeRepositoryImpl(
     private val fuelTypeService: FuelTypeService,
     private val fuelTypeProvider: FuelTypeProvider,
 ) : FuelTypeRepository {
+    @Suppress("UNCHECKED_CAST")
     override suspend fun getAllFuelTypes(): List<FuelType> {
         try {
-            //FIXME Change to get data from API
-            var list = ArrayList<FuelType>()
-            if (fuelTypeProvider.getFuelTypesState().isEmpty()) {
-//                for (fuelType in fuelTypeService.getAllFuelTypes()) {
-//                    list.add(FuelTypeResponse.mapToDomain(fuelType))
-//                }
-                list.add(FuelType("1", "95"))
-                list.add(FuelType("2", "92", true))
-                list.add(FuelType("3", "ДП", true))
-                list.add(FuelType("4", "Газ"))
-                fuelTypeProvider.saveFuelTypesState(list)
-            } else {
-                list = fuelTypeProvider.getFuelTypesState().toList() as ArrayList<FuelType>
+            val sharedPreferencesList = fuelTypeProvider.getFuelTypesState()
+            val apiList = fuelTypeService.getAllFuelTypes()
+                .map { fuelType -> FuelTypeResponse.mapToDomain(fuelType) }
+            if (sharedPreferencesList.isEmpty()) {
+                fuelTypeProvider.saveFuelTypesState(apiList)
+            } else if (sharedPreferencesList.map{ it.id }!=apiList.map{ it.id }) {
+                val onlyNew=apiList.map { apiType-> !sharedPreferencesList.map{ sharedType->sharedType.id}.contains(apiType.id) }
+                fuelTypeProvider.saveFuelTypesState((sharedPreferencesList+ onlyNew) as List<FuelType>)
             }
-            return list
+            return fuelTypeProvider.getFuelTypesState()
         } catch (e: Throwable) {
             throw e.mapToApiErrors()
         }
